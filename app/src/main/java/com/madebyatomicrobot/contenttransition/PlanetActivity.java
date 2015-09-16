4237950b373e4d9661c01f30fef5be53a8f5b660
Adam Toennis <adam@madebyatomicrobot.com>
2015-09-16 08:53:41 -0400

    Updated layout name.

--- app/src/main/java/com/madebyatomicrobot/contenttransition/PlanetActivity.java
+++ app/src/main/java/com/madebyatomicrobot/contenttransition/PlanetActivity.java
@@ -35,7 +35,7 @@ public class PlanetActivity extends AppCompatActivity implements OnPlanetClicked
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_planet);
 
         PlanetAdapter planetAdapter = new PlanetAdapter(this);
         planetAdapter.setPlanets(planets);
